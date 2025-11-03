54c7b9a3417d0fe8939401c7194bfc30f26bcfd6
chabanechaouchemohand004-ctrl <chabanechaouchemohand004@gmail.com>
2025-11-03 07:30:02 +0100

    ajout de methode chien

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("hello World")
+from test import chein
+c=chein()
+c.afficher() # This will print: Hello, Alice!
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
 print("hello GitHub")
+class chein: 
+    def afficher(self):
+        print("je suis un chien ")
+
+   
